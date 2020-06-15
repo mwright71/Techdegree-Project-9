23b2831ed7f08b6776473532573c0f42f0a3440e
Matthew Wright <mwright71@gmail.com>
2020-06-15 09:47:00 -0500

    added directory

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const dashboard = document.getElementById('dashboard');
 const wheelGame = document.getElementById('wheel');
 const form = document.getElementById('form');
 const gallery = document.getElementById('gallery');
+const directory = document.getElementById('directory');
 
 function toggleDisplay(button, className) {
     if(event.target.className === className) {
@@ -17,4 +18,5 @@ buttons.addEventListener('click', e => {
     toggleDisplay(wheelGame, 'wheelBtn');
     toggleDisplay(form, 'formBtn');
     toggleDisplay(gallery, 'galleryBtn');
+    toggleDisplay(directory, 'directoryBtn');
 });
