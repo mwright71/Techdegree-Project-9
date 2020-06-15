e5863388a2a3c670bb041218a20a24bd1194b8b7
Matthew Wright <mwright71@gmail.com>
2020-06-15 12:53:30 -0500

    added skills

--- js/script.js
+++ js/script.js
@@ -20,3 +20,17 @@ buttons.addEventListener('click', e => {
     toggleDisplay(gallery, 'galleryBtn');
     toggleDisplay(directory, 'directoryBtn');
 });
+
+$('#owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: true,
+    nav: true,
+    items: 2,
+})
+  
+
+  
+
+
+ 
---
+++ js/skills.js
@@ -0,0 +1,11 @@
+$('#owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: true,
+    nav: true,
+    mouseDrag: true,
+    touchDrag: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    items: 1,
+});
