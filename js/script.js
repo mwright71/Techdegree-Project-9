63bb8433160f2bc7f715721d319c56959ed14c88
Matthew Wright <mwright71@gmail.com>
2020-06-16 11:34:44 -0500

    fixed layout issues and validation errors

--- js/script.js
+++ js/script.js
@@ -21,13 +21,7 @@ buttons.addEventListener('click', e => {
     toggleDisplay(directory, 'directoryBtn');
 });
 
-$('#owl-carousel').owlCarousel({
-    loop: true,
-    margin: 30,
-    dots: true,
-    nav: true,
-    items: 2,
-})
+
   
 
   
